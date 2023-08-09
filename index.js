df9b0e50715780fbe916b16fc531a35b17d9ac27
AMEL BATOUCHE <amelbat7400@gmail.com>
2023-08-09 20:26:25 +0200

    Objects Assignement Completed

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+const employee = {
+name: "Amel",
+streetAddress: "Mascara"
+}
+function updateEmployeeWithKeyAndValue(obj, key, value){
+const newEmployee = { ...employee, name: "Sam", streetAddress: "11 Broadway"}
+return newEmployee;
+};
+function destructivelyUpdateEmployeeWithKeyAndValue(){
+  employee.name = "Sam";
+  employee.streetAddress = "12 Broadway";
+  return employee;
+}
+function destructivelyDeleteFromEmployeeByKey(){
+  delete employee.name;
+  return employee;
+}
+function deleteFromEmployeeByKey(employee, key) {
+  const newlyEmployee = { ...employee};
+  delete newlyEmployee.name;
+  return newlyEmployee
+}
